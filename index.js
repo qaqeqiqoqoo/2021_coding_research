172589b0060db4593b5b103f12f92ed4bc9581a5
qaqeqiqoqoo <72537392+qaqeqiqoqoo@users.noreply.github.com>
2021-03-14 15:20:37 +0900

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const toggleBt = document.querySelector(".navbar_toggleBt");
-const menu = document.querySelector(".navbar_menu");
-const icon = document.queryCommandValue(".navbar_icon");
-
-toggleBt.addEventListener("click", () => {
-menu.classList.toggle("active");
-icon.classList.toggle("active");
-
-});
